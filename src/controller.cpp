6da2ca12a549fdeefae2173ff5e8e707aa2deb2c
andreasmpoulsen <andreas_poulsen@hotmail.com>
2021-04-22 16:54:41 +0200

    Controller update

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,7 +1,11 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Empty.h>
+#include <geometry_msgs/Twist.h>
+#include <iostream>
+#include <string>
 
+using namespace std;
 using namespace ros;
 
 int main(int argc, char **argv)
@@ -9,10 +13,27 @@ int main(int argc, char **argv)
     init(argc, argv, "controller");
     NodeHandle n;
     Publisher takeoff_pub = n.advertise<std_msgs::Empty>("ardrone/takeoff", 10);
+    Publisher land_pub = n.advertise<std_msgs::Empty>("ardrone/land", 10);
+    Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
     std_msgs::Empty msg;
+    geometry_msgs::Twist vel;
+    string choice;
     while (ros::ok())
     {
-        takeoff_pub.publish(msg);
+        cin >> choice;
+        if (choice == "t")
+        {
+            takeoff_pub.publish(msg);
+        }
+        else if (choice == "l")
+        {
+            land_pub.publish(msg);
+        }
+        else
+        {
+            vel.linear.x = stoi(choice);
+            vel_pub.publish(vel);
+        }
         spinOnce();
     }
     return 0;
