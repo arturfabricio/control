5a2093a41d722e416353116d6980680e59586d3f
andreasmpoulsen <andreas_poulsen@hotmail.com>
2021-04-27 15:00:31 +0200

    Added PCL filter

--- src/position.cpp
+++ src/position.cpp
@@ -23,19 +23,23 @@ double least_distance = 10000;
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 
-void measure_distance (std::vector<float> &x, std::vector<float> &y, std::vector<float> &z){
+void measure_distance(std::vector<float> &x, std::vector<float> &y, std::vector<float> &z)
+{
     std::vector<double> distance;
     least_distance = 10000;
-    for(int i = 0; i < x.size(); i++){
-        distance.push_back(sqrt(pow((x_current-x[i]),2)+(pow((y_current-y[i]),2))+(pow((z_current-z[i]),2))));
-        if(distance[i] < least_distance){
+    for (int i = 0; i < x.size(); i++)
+    {
+        distance.push_back(sqrt(pow((x_current - x[i]), 2) + (pow((y_current - y[i]), 2)) + (pow((z_current - z[i]), 2))));
+        if (distance[i] < least_distance)
+        {
             least_distance = distance[i];
         }
     }
     std::cout << "Least Distance: " << least_distance << "\n";
 }
 
-void tf_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
+void tf_callback(const geometry_msgs::PoseStamped::ConstPtr &msg)
+{
     // ROS_INFO_STREAM("Received pose: " << msg);
     x_current = msg->pose.position.x;
     y_current = msg->pose.position.y;
@@ -44,12 +48,14 @@ void tf_callback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
     pose.push_back(msg);
 }
 
-void xyz_callback(const PointCloud::ConstPtr& msg){
+void xyz_callback(const PointCloud::ConstPtr &msg)
+{
     std::vector<float> x_obstacle;
     std::vector<float> y_obstacle;
     std::vector<float> z_obstacle;
     //printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
-    BOOST_FOREACH(const pcl::PointXYZ& pt, msg->points){
+    BOOST_FOREACH (const pcl::PointXYZ &pt, msg->points)
+    {
         //printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
         x_obstacle.push_back(pt.x);
         y_obstacle.push_back(pt.y);
@@ -58,16 +64,17 @@ void xyz_callback(const PointCloud::ConstPtr& msg){
     measure_distance(x_obstacle, y_obstacle, z_obstacle);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "my_subscriber");
     ros::NodeHandle nh;
     ros::Subscriber subscribetf = nh.subscribe("/orb_slam2_mono/pose", 1000, tf_callback); //Topic_name, queue size and callback function.
-    ros::Subscriber subscriverpc = nh.subscribe("/orb_slam2_mono/map_points", 1, xyz_callback);
+    ros::Subscriber subscriverpc = nh.subscribe("/statisticalOutliers/output", 1, xyz_callback);
     // ros::Publisher point_pub = nh.advertise<pcl::PointXYZ>("/points", 1000);
-    
+
     // while(ros::ok()){
     // point_pub.publish(pt);
     ros::spin();
     // }
-    return(0);
+    return (0);
 }
